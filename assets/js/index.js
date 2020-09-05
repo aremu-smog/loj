306e9ee85dcbcb2622c0522772377b70ad91220d
aremu-smog <aremuoluwagbamila@gmail.com>
2020-09-05 00:27:26 +0000

    Feat: Navbar background and padding changes based on scrolling

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,21 @@ nav_menu.addEventListener("click", () => {
 }, false)
 // document.addEventListener("load", ()=>{
 // })
+
+const main_header = document.querySelector(".main-header")
+const main_header_container = main_header.querySelector(".container")
+
+var scroll_amount
+document.addEventListener('scroll', ()=>{
+    scroll_amount = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    if(scroll_amount > 536){
+        main_header.style.backgroundColor = "rgba(25, 39, 81, 1)"
+        main_header.style.padding = "0"
+        main_header_container.style.padding = "1em"
+    }else{
+        main_header.style.padding = "2em 0"
+        main_header.style.backgroundColor = "rgba(25, 39, 81, 0)"
+        main_header_container.style.padding = "2em"
+    }
+   
+})
